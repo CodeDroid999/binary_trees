e32c24f883f12498e4b0ab4e360cbe19a63e01d4
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-27 09:59:45 +0300

    132-array_to_heap.c

---
+++ 132-array_to_heap.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_heap - Builds a Max Binary Heap tree from an array.
+ *
+ * @array: A pointer to the first element of the array to be converted.
+ * @size: The number of element in the array.
+ *
+ * Return: A pointer to the root node of the created one, NULL on failure.
+ */
+heap_t *array_to_heap(int *array, size_t size)
+{
+	unsigned int index;
+	heap_t *root = NULL;
+
+	for (index = 0; index < size; index++)
+		heap_insert(&root, array[index]);
+
+	return (root);
+}
